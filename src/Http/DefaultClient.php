c2175a56d0246e826bda817d11ace704e68dcd02
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-14 21:13:37 +0500

    Default JSON RPC client

--- src/Http/DefaultClient.php
+++ src/Http/DefaultClient.php
@@ -14,14 +14,84 @@ declare(strict_types=1);
 
 namespace BitcoinRPC\Http;
 
+use BitcoinRPC\Exception\BitcoinRPCException;
+use BitcoinRPC\Exception\ConnectionException;
+use BitcoinRPC\Exception\DaemonException;
+use HttpClient\Exception\JSON_RPC_Exception;
+use HttpClient\JSON_RPC;
+
 /**
  * Class DefaultClient
  * @package BitcoinRPC\Http
  */
 class DefaultClient extends AbstractJSONClient
 {
+    /** @var JSON_RPC */
+    private $jsonRPC;
+
+    /**
+     * DefaultClient constructor.
+     * @param string $host
+     * @param int $port
+     * @throws \HttpClient\Exception\JSON_RPC_Exception
+     */
+    public function __construct(string $host, int $port)
+    {
+        parent::__construct($host, $port);
+
+        $this->jsonRPC = new JSON_RPC("1.0");
+        $this->jsonRPC->server($host, $port);
+    }
+
+    /**
+     * @param string $httpMethod
+     * @param string $endpoint
+     * @param string $id
+     * @param string $method
+     * @param array|null $params
+     * @return DaemonResponse
+     * @throws BitcoinRPCException
+     * @throws ConnectionException
+     * @throws DaemonException
+     */
     public function jsonRPC_call(string $httpMethod, string $endpoint, string $id, string $method, ?array $params = null): DaemonResponse
     {
+        // Set credentials
+        $this->jsonRPC->authentication()
+            ->basic($this->auth->username, $this->auth->password);
 
+        // Prepare & Send Request
+        try {
+            $res = $this->jsonRPC->request($httpMethod, $endpoint)
+                ->id($id)
+                ->method($method)
+                ->params($params)
+                ->send();
+        } catch (JSON_RPC_Exception $e) {
+            throw new ConnectionException($e->getMessage(), $e->getCode());
+        } catch (\Exception $e) {
+            throw new BitcoinRPCException(
+                sprintf('[%s][#%d]: %s', get_class($e), $e->getCode(), $e->getMessage())
+            );
+        }
+
+        // Has error?
+        if (!$res->result) {
+            $errorMessage = $res->error->message;
+            if (!$errorMessage) {
+                $errorMessage = 'An unknown error occurred, failed to retrieve "result"';
+            }
+
+            throw new DaemonException($errorMessage, $res->error->code);
+        }
+
+        // Compile DaemonResponse
+        $resHttpQuery = $res->http();
+
+        $daemonResponse = new DaemonResponse($resHttpQuery->headers(), $resHttpQuery->array());
+        $daemonResponse->httpStatusCode = $resHttpQuery->code();
+        $daemonResponse->id = $res->id;
+        $daemonResponse->result = $res->result;
+        return $daemonResponse;
     }
 }
