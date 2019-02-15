38eb5bdb3b8e583352aebf4a3d5abc9c63136f39
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-15 20:23:19 +0500

    createWallet method

--- src/Client/Wallets.php
+++ src/Client/Wallets.php
@@ -43,6 +43,38 @@ class Wallets
     }
 
     /**
+     * @param string $name
+     * @param bool $suppressWarning
+     * @return Wallet
+     * @throws WalletsException
+     */
+    public function create(string $name, bool $suppressWarning = false): Wallet
+    {
+        if (!preg_match('/^[\w\-]{3,40}$/i', $name)) {
+            throw new WalletsException('Invalid wallet name to create');
+        }
+
+        $res = $this->bitcoinRPC->jsonRPC_client()->jsonRPC_call("createWallet", null, [$name]);
+        $createdWallet = $res->result;
+        if (!is_array($createdWallet) || !array_key_exists("name", $createdWallet)) {
+            throw new WalletsException('Failed to create new wallet, invalid response');
+        }
+
+        if ($createdWallet["name"] !== $name) {
+            throw new $createdWallet('Create wallet name does not match with request');
+        }
+
+        $warning = $loadWallet["warning"] ?? null;
+        if ($warning) {
+            if (!$suppressWarning) {
+                throw new $createdWallet(sprintf('Create wallet [%s] warning: %s', $name, $warning));
+            }
+        }
+
+        return $this->get($name);
+    }
+
+    /**
      * @param string|null $name
      * @return Wallet
      * @throws \BitcoinRPC\Exception\WalletsException
